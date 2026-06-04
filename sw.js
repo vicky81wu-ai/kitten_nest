e465da62ffcb579193dfd46a2d09048e3e35edc5
vicky81wu-ai <vicky81wu@gmail.com>
2026-06-05 01:41:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-const CACHE_NAME = "kitten-nest-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "kitten-nest-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.png",
+  "./apple-touch-icon.png"
+];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
